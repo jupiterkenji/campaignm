a7492e1e85aca77def82a924bc3dcfa50907e75c
Jony Kwa <jony_kwa@hotmail.com>
2018-12-07 20:59:15 +1100

    fix all test

--- campaignm/Utilities.cs
+++ campaignm/Utilities.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -12,21 +13,67 @@ namespace campaignmonitor
         }
 
         // Question 2
-        public static IEnumerable<int> GetPositiveDivisor(int number)
+        public static int[] GetPositiveDivisor(int number)
         {
-            return Enumerable.Empty<int>();
+            if (number < 0)
+            {
+                throw new ArgumentException("number should be positive");
+            }
+
+            var result = new List<int>();
+            for (var i=1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    result.Add(i);
+                }
+            }
+
+            return result.ToArray();
         }
 
         // Question 3
-        public static int GetTriangleArea(int lenght1, int length2, int length3)
+        public static double GetTriangleArea(int length1, int length2, int length3)
         {
-            return -1;
+            if (length1 < 1 || length2 < 1 || length3 < 1)
+            {
+                throw new InvalidTriangleException("Length should be greater than 0");
+            }
+
+            var isValid = length1+length2 > length3
+                && length1+length3 > length2
+                && length2+length3 > length1;
+            if(!isValid)
+            {
+                throw new InvalidTriangleException("This is not a valid triangle");
+            }
+
+            var p = (length1+length2+length3) / 2;
+
+            return Math.Sqrt(p * (p-length1) * (p-length2) * (p-length3));
         }
 
         // Question 4
         public static IEnumerable<int> GetMostCommon(IEnumerable<int> numbers)
         {
-            return Enumerable.Empty<int>();
+            var result = new Dictionary<int, int>();
+
+            foreach (var number in numbers)
+            {
+                if (result.ContainsKey(number))
+                {
+                    result[number]++;
+                }
+                else
+                {
+                    result[number] = 0;
+                }
+            }
+
+            var maxOccurrence = result.Values.Max();
+            return result
+                .Where(kvp => kvp.Value == maxOccurrence)
+                .Select(kvp => kvp.Key);
         }
     }
 }
--- test/Test.cs
+++ test/Test.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using campaignmonitor;
 using Xunit;
 
@@ -11,19 +12,19 @@ namespace test
         {
             Assert.Equal(true, Utilities.IsNullOrEmpty(null));
             Assert.Equal(false, Utilities.IsNullOrEmpty("a"));
-            Assert.Equal(false, Utilities.IsNullOrEmpty(""));
+            Assert.Equal(true, Utilities.IsNullOrEmpty(""));
             Assert.Equal(false, Utilities.IsNullOrEmpty("null"));
         }
 
         [Fact]
         public void GetPositiveDivisorTest()
         {
-            Assert.Equal(new[] {1, 2, 3, 4, 5, 7, 10, 12, 15, 20, 30, 60}, Utilities.GetPositiveDivisor(60));
+            Assert.Equal(new[] {1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30, 60}, Utilities.GetPositiveDivisor(60));
             Assert.Equal(new[] {1, 2, 3, 6, 7, 14, 21, 42}, Utilities.GetPositiveDivisor(42));
 
             Assert.Equal(new int[0], Utilities.GetPositiveDivisor(0));
 
-            //jk-todo: negative?
+            Assert.Throws<ArgumentException>( () => Utilities.GetPositiveDivisor(-1) );
         }
 
 
@@ -40,15 +41,13 @@ namespace test
             Assert.Throws<InvalidTriangleException>( () => Utilities.GetTriangleArea(-3, 4, -5) );
             Assert.Throws<InvalidTriangleException>( () => Utilities.GetTriangleArea(-3, -4, -5) );
 
-            // jk-todo: invalid triangle
-            //Assert.Throws<InvalidTriangleException>( () => Utilities.GetTriangleArea(1, 2, 1) );
+            Assert.Throws<InvalidTriangleException>( () => Utilities.GetTriangleArea(1, 10, 12) );
         }
 
         [Fact]
         public void GetMostCommonTest()
         {
-            //jk-todo: nay order
-            Assert.Equal(new[] {5, 4}, Utilities.GetMostCommon(new[] {5, 4, 3, 2, 4, 5, 1, 6, 1, 2, 5, 4}));
+            Assert.Equal(new[] {4, 5}, Utilities.GetMostCommon(new[] {5, 4, 3, 2, 4, 5, 1, 6, 1, 2, 5, 4}).OrderBy(x => x));
             Assert.Equal(new[] {1}, Utilities.GetMostCommon(new[] {1, 2, 3, 4, 5, 1, 6, 7}));
             Assert.Equal(new[] {1, 2, 3, 4, 5, 6, 7}, Utilities.GetMostCommon(new[] {1, 2, 3, 4, 5, 6, 7}));
         }
